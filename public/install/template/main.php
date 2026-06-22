9b9c7ccdf04bf0240fb6d47c019a3699add2e8bb
likeadmin <416716328@qq.com>
2026-06-22 20:31:49 +0800

    fix: remove env precheck from installer

--- public/install/template/main.php
+++ public/install/template/main.php
@@ -339,12 +339,6 @@
                                                 <?php echo $modelInstall->correctOrFail($modelInstall->checkDirWrite('config')) ?>
                                                 <td><?php if ($modelInstall->checkDirWrite('config') == 'fail') echo '请给config目录权限，若目录不存在先新建'; ?></td>
                                             </tr>
-                                            <tr>
-                                                <td>../.env</td>
-                                                <td>.env文件可写或可创建</td>
-                                                <?php echo $modelInstall->correctOrFail($modelInstall->checkDirWrite('.env')) ?>
-                                                <td><?php if ($modelInstall->checkDirWrite('.env') == 'fail') echo '请给.env文件权限；若文件不存在，请确保网站根目录可写，或手动创建.env后授权'; ?></td>
-                                            </tr>
                                             </tbody>
                                         </table>
                                     </div>
