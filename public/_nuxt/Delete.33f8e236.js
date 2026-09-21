a0e1155e43ba34782a866a4ecb32dbab84d16027
likeadmin <likeadmin@likeadmindeMacBook-Pro.local>
2026-09-21 09:54:04 +0800

    release: publish system version 1.0.342

---
+++ public/_nuxt/Delete.33f8e236.js
@@ -0,0 +1,1 @@
+import"./entry.ff53fad2.js";const o=""+globalThis.__publicAssetsURL("short-drama/figma/icon/Copy.svg"),a=""+globalThis.__publicAssetsURL("short-drama/figma/icon/Delete.svg");export{o as _,a};
--- public/_nuxt/castArray.6d663364.js
+++
@@ -1,1 +0,0 @@
-import{cm as a}from"./entry.94160188.js";function e(){if(!arguments.length)return[];var r=arguments[0];return a(r)?r:[r]}export{e as c};
