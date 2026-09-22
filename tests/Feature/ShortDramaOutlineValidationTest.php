1a8c2c5c2f8338a5a79e3bf97d4a8436d2ae146c
Uana <panda@UanaMacBook-Air.local>
2026-09-22 18:38:54 +0800

    test: assert outline-only payload shape

--- tests/Feature/ShortDramaOutlineValidationTest.php
+++ tests/Feature/ShortDramaOutlineValidationTest.php
@@ -20,8 +20,8 @@ class ShortDramaOutlineValidationTest extends TestCase
         self::assertContains('outline.episode.missing', array_column($result['outline_validation_issues'], 'code'));
         self::assertSame([], $result['storyboard']);
         self::assertSame([], $result['duration_stats']);
-        self::assertSame([], $result['episodes'][1]['scenes']);
-        self::assertSame([], $result['episodes'][1]['shots']);
+        self::assertArrayNotHasKey('scenes', $result['episodes'][1]);
+        self::assertArrayNotHasKey('shots', $result['episodes'][1]);
     }
 
     public function testMissingShotsAreNotClonedIntoOtherEpisodes(): void
